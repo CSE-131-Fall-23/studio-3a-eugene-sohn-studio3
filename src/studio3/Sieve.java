204d6a2281a435c4f8a7c9f0c45435ef021083b1
raphamitterer-claudet <raphamitterer-claudet@localhost>
2023-09-20 15:50:23 -0500

    Studio 3

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,47 @@
+package studio3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("How many numbers do you want in the sieve? ");
+		int n = in.nextInt();
+		boolean[]list = new boolean[n];
+		boolean sieve = false; 
+		boolean isPrime = false; 
+		int j;
+		int p = 2;
+		while(!sieve) {
+			p += p;
+			for(int i = p; i < list.length; i+=p) {
+				list[i] = true;
+			}
+			j = 1;
+			if(p <= Math.sqrt(n)) {
+				sieve = true;
+			}
+			while(isPrime == false) {
+				if(list[p+j] == false) {
+					System.out.println(p + j + " is prime");
+					isPrime = true;
+					p = p + j;
+				}
+				j++;
+			}
+			isPrime = false;
+		
+
+	}
+		System.out.println(Arrays.toString(list));
+		for(int i = 0; i < list.length; i++) {
+			if(list[i] == false) {
+				System.out.println(i);
+			}
+		}
+}
+}
+
